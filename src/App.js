9cd4f45d0a1dd1dc14d5402dbafbfbf755a5a55f
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-06-13 07:50:22 +0700

    MC-8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import Home from './Components/Hompage/Homepage';
+import LoginForm from './Components/Auth/LoginPage';
+import Register from './Components/Auth/RegisterPage';
+import VerifyCode from './Components/Auth/VerifyCodePage';
 import "./Styles/global.css";
+import UserInfo from './Components/Auth/UserInfo';
 function App() {
   return (
     
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/Login" element={<LoginForm/>} />
+      <Route path="/Register" element={<Register/>} />
+      <Route path="/VerifyCode" element={<VerifyCode/>} />
+      <Route path="/UserInfo" element={<UserInfo/>} />
     </Routes>    
   );
 }
 
 export default App;
+
