32fdb044b21efca1c9fa483b88797734e718c6a7
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-06-15 01:04:12 +0700

    MC-8

--- src/Components/Auth/UserInfo.js
+++ src/Components/Auth/UserInfo.js
@@ -4,37 +4,35 @@ import axios from "axios";
 
 const UserInfo = () => {
   const navigate = useNavigate();
-  const [name, setName] = useState("");
+  const [full_name, setFullName] = useState("");
   const [phone, setPhone] = useState("");
-  const [nameError, setNameError] = useState("");
+  const [full_nameError, setNameError] = useState("");
   const [phoneError, setPhoneError] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!/^[a-zA-Z\s]*$/.test(name)) {
-      setNameError("Only letters and white space allowed");
+    if (!/^[a-zA-Z\s]*$/.test(full_name)) {
+      setNameError("Chỉ cho phép chữ cái và khoảng trắng");
     } else if (!/^[0-9]{10}$/.test(phone)) {
-      setPhoneError("Invalid phone number");
+      setPhoneError("Số điện thoại không hợp lệ");
     } else {
       // Gửi yêu cầu lưu thông tin đến API
       axios
-        .post("https://647783419233e82dd53bc684.mockapi.io/mypham/users", { name, phone })
+        .post("http://127.0.0.1:8000/api/users", { full_name, phone })
         .then((response) => {
-          // Xử lý phản hồi thành công từ API
-          if (response.data.success) {
-            // Chuyển hướng đến trang đăng nhập hoặc trang khác tùy thuộc vào yêu cầu
-            navigate("/login"); // Chuyển hướng đến đường dẫn "/login"
+          if (response.status === 200) {
+            alert('Xác thực thành công, Vui lòng đăng nhập!');
+            navigate("/Login");
           } else {
-            // Xử lý phản hồi lỗi từ API
-            // Ví dụ: hiển thị thông báo lỗi
             console.log(response.data.error);
+            setErrorMessage("Đã xảy ra lỗi khi lưu thông tin người dùng. Vui lòng thử lại.");
           }
         })
         .catch((error) => {
-          // Xử lý lỗi khi gửi yêu cầu đến API
-          // Ví dụ: hiển thị thông báo lỗi
           console.log(error.message);
+          setErrorMessage("Đã xảy ra lỗi khi lưu thông tin người dùng. Vui lòng thử lại.");
         });
     }
   };
@@ -53,8 +51,8 @@ const UserInfo = () => {
               className="form-control"
               id="name"
               name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={full_name}
+              onChange={(e) => setFullName(e.target.value)}
               required
             />
           </div>
@@ -64,7 +62,7 @@ const UserInfo = () => {
           <br />
           <div className="form-input">
             <input
-              type="phone"
+              type="tel"
               className="form-control"
               id="phone"
               name="phone"
@@ -75,20 +73,19 @@ const UserInfo = () => {
           </div>
         </div>
 
-        <button type="submit" className="btn btn-default" name="btn">Save</button>
-<div>
-{nameError && <span className="error">{nameError}</span>}
-{phoneError && <span className="error">{phoneError}</span>}
-<br />
-<br />
-</div>
-</form>
-</div>
-);
+        <button type="submit" className="btn btn-default" name="btn">
+          Save
+        </button>
+        <div>
+          {full_nameError && <span className="error">{full_nameError}</span>}
+          {phoneError && <span className="error">{phoneError}</span>}
+          {errorMessage && <span className="error">{errorMessage}</span>}
+          <br />
+          <br />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default UserInfo;
-
-
-
-
--- src/Components/Auth/VerifyCodePage.js
+++ src/Components/Auth/VerifyCodePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 import '../Auth/VerifyCodePage.css';
@@ -6,10 +6,11 @@ import '../../Styles/global.css';
 
 const VerifyCode = () => {
   const [email, setEmail] = useState('');
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState(['', '', '', '', '', '']);
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
   const location = useLocation();
+  const inputRefs = useRef([]);
 
   useEffect(() => {
     // Lấy email từ query param trong URL
@@ -22,17 +23,22 @@ const VerifyCode = () => {
     const newCode = [...code];
     newCode[index] = value;
     setCode(newCode);
+
+    if (value !== '' && index < 5) {
+      // Move focus to the next input
+      inputRefs.current[index + 1].focus();
+    }
   };
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       const response = await axios.post('http://127.0.0.1:8000/api/verify', {
         email,
         otp: code.join(''), // Chuyển đổi mảng code thành chuỗi
       });
-  
+
       if (response.status === 200) {
         alert('Xác minh thành công');
         navigate('/UserInfo');
@@ -42,10 +48,8 @@ const VerifyCode = () => {
     } catch (error) {
       console.error(error.response);
       setErrorMessage('Đã xảy ra lỗi khi xác minh mã. Vui lòng thử lại');
-      
     }
   };
-  
 
   const handleResend = async () => {
     try {
@@ -54,7 +58,6 @@ const VerifyCode = () => {
       if (response.data.length > 0) {
         const resendResponse = await axios.post(`http://127.0.0.1:8000/api/resend/${response.data[0].id}`);
         if (resendResponse.status === 200) {
-         
           alert('Resend successful');
           // Chèn phần logic bổ sung nếu cần thiết ở đây
         } else {
@@ -75,7 +78,7 @@ const VerifyCode = () => {
           &nbsp;
           <p className="info">An OTP has been sent to {email}</p>
           <div className="code-container">
-            {[...Array(6)].map((_, index) => (
+          {[...Array(6)].map((_, index) => (
               <input
                 key={index}
                 type="number"
@@ -84,8 +87,8 @@ const VerifyCode = () => {
                 min="0"
                 max="9"
                 required
-                value={code[index] || ''}
-                onChange={(e) => handleCodeChange(index, e.target.value)}
+                value={code[index]}onChange={(e) => handleCodeChange(index, e.target.value)}
+                ref={(input) => (inputRefs.current[index] = input)}
               />
             ))}
           </div>
