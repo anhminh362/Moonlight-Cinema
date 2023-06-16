a53799318d36b4689388f89cbd43ebfda9cc2da0
anhminh362 <116275383+anhminh362@users.noreply.github.com>
2023-06-16 13:26:20 +0700

    merge code update

--- src/Components/Auth/VerifyCodePage.js
+++ src/Components/Auth/VerifyCodePage.js
@@ -4,63 +4,63 @@ import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 import '../Auth/verifycodepages.css';
 
-  const handleResend = async () => {
-    try {
-      const response = await axios.get(`https://647783419233e82dd53bc684.mockapi.io/mypham/account?email=${email}`);
+  // const handleResend = async () => {
+  //   try {
+  //     const response = await axios.get(`https://647783419233e82dd53bc684.mockapi.io/mypham/account?email=${email}`);
 
-      if (response.data.length > 0) {
-        const resendResponse = await axios.post(`https://647783419233e82dd53bc684.mockapi.io/mypham/resend/${response.data[0].id}`);
-        if (resendResponse.status === 200) {
-          alert('The verification code has been resent');
-        } else {
-          setErrorMessage('An error occurred while resending the verification code. Please try again');
-        }
-      }
-    } catch (error) {
-      console.error(error);
-      setErrorMessage('An error occurred while resending the verification code. Please try again');
-    }
-  };
+  //     if (response.data.length > 0) {
+  //       const resendResponse = await axios.post(`https://647783419233e82dd53bc684.mockapi.io/mypham/resend/${response.data[0].id}`);
+  //       if (resendResponse.status === 200) {
+  //         alert('The verification code has been resent');
+  //       } else {
+  //         setErrorMessage('An error occurred while resending the verification code. Please try again');
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //     setErrorMessage('An error occurred while resending the verification code. Please try again');
+  //   }
+  // };
 
-  return (
-    <div className="form-register">
-      <form onSubmit={handleFormSubmit}>
-        <div className="container">
-          <h2>Verify Your Account</h2>
-          &nbsp;
-          <p className="info">An OTP has been sent to {email}</p>
-          <div className="code-container">
+  // return (
+  //   <div className="form-register">
+  //     <form onSubmit={handleFormSubmit}>
+  //       <div className="container">
+  //         <h2>Verify Your Account</h2>
+  //         &nbsp;
+  //         <p className="info">An OTP has been sent to {email}</p>
+  //         <div className="code-container">
            
-            {[...Array(5)].map((_, index) => (
-              <input
-                key={index}
-                type="number"
-                className="code"
-                placeholder="0"
-                min="0"
-                max="9"
-                required
-                value={code[index] || ''}
-                onChange={(e) => {
-                  const newCode = [...code];
-                  newCode[index] = e.target.value;
-                  setCode(newCode);
-                }}
-              />
-            ))}
-          </div>
-          {errorMessage && <div className="error-message">{errorMessage}</div>}
-          <button type="submit" className="btn btn-default btn__verify" name="btn">
-            Verify
-          </button>
-          <button type="button" className="btn btn-link btn__resend" onClick={handleResend}>
-            Resend Code
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-};
+  //           {[...Array(5)].map((_, index) => (
+  //             <input
+  //               key={index}
+  //               type="number"
+  //               className="code"
+  //               placeholder="0"
+  //               min="0"
+  //               max="9"
+  //               required
+  //               value={code[index] || ''}
+  //               onChange={(e) => {
+  //                 const newCode = [...code];
+  //                 newCode[index] = e.target.value;
+  //                 setCode(newCode);
+  //               }}
+  //             />
+  //           ))}
+  //         </div>
+  //         {errorMessage && <div className="error-message">{errorMessage}</div>}
+  //         <button type="submit" className="btn btn-default btn__verify" name="btn">
+  //           Verify
+  //         </button>
+  //         <button type="button" className="btn btn-link btn__resend" onClick={handleResend}>
+  //           Resend Code
+  //         </button>
+  //       </div>
+  //     </form>
+  //   </div>
+  // );
+
 
 
 const VerifyCode = () => {
@@ -131,7 +131,7 @@ const VerifyCode = () => {
 
   return (
     <div className='body'>
-    <div className="form-register">
+    <div className="form-register" >
       <form onSubmit={handleFormSubmit}>
         <div className="container">
           <h2>Verify Your Account</h2>
