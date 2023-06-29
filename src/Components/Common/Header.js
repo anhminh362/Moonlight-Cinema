ec30fe702abb73cdd4dd7075b626c23801823e44
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-06-29 09:56:49 +0700

    MC-111

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -53,10 +53,10 @@ const Header = () => {
 						<a href="#">Movies</a>
 						<ul id="type-movies">
 							<li>
-								<a href="playing.php">Playing</a>
+								<a href="/Playing">Playing</a>
 							</li>
 							<li>
-								<a href="upcoming.php">Upcoming</a>
+								<a href="/Upcomingg">Upcoming</a>
 							</li>
 						</ul>
 					</li>
