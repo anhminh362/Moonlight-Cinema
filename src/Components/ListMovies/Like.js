5cc93317d138f86a16b8ef9c8f65054202ed4893
KieuMei2811 <hothikieu28112003@gmail.com>
2023-07-03 00:34:47 +0700

    updatedd

--- src/Components/ListMovies/Like.js
+++ src/Components/ListMovies/Like.js
@@ -33,36 +33,57 @@ const Like = () => {
 
 	const saveLike = () => {
 		if (movies && users) {
+		  const requestData = { 'movie_id': movies, 'user_id': users };
+	  		  if (isLike) {
+			// Gửi request DELETE để xóa lượt thích
 			fetch("http://127.0.0.1:8000/api/like", {
-				method: 'POST',
-				body: JSON.stringify({ 'movie_id': movies, 'user_id': users }),
-				headers: {
-					'Content-Type': 'application/json',
-				},
+			  method: 'DELETE',
+			  body: JSON.stringify(requestData),
+			  headers: {
+				'Content-Type': 'application/json',
+			  },
 			})
-				.then(response => response.json())
-				.then(data => {
-					console.log('Lượt thích đã được lưu vào cơ sở dữ liệu:', data);
-				})
-				.catch(error => {
-					console.error('Lỗi khi lưu lượt thích vào cơ sở dữ liệu:', error);
-				});
+			  .then(response => response.json())
+			  .then(data => {
+				console.log('Lượt thích đã được xóa khỏi cơ sở dữ liệu:', data);
+			  })
+			  .catch(error => {
+				console.error('Lỗi khi xóa lượt thích khỏi cơ sở dữ liệu:', error);
+			  });
+		  } else {
+			// Gửi request POST để thêm lượt thích mới
+			fetch("http://127.0.0.1:8000/api/like", {
+			  method: 'POST',
+			  body: JSON.stringify(requestData),
+			  headers: {
+				'Content-Type': 'application/json',
+			  },
+			})
+			  .then(response => response.json())
+			  .then(data => {
+				console.log('Lượt thích đã được lưu vào cơ sở dữ liệu:', data);
+			  })
+			  .catch(error => {
+				console.error('Lỗi khi lưu lượt thích vào cơ sở dữ liệu:', error);
+			  });
+		  }
 		}
-	};
-
-	const onLikeClick = () => {
+	  };
+	  
+	  const onLikeClick = () => {
 		if (isLoggedIn) {
-			setLike(like + (isLike ? -1 : 1));
-			setIsLike(!isLike);
-			saveLike();
+		  setLike(like + (isLike ? -1 : 1));
+		  setIsLike(!isLike);
+		  saveLike();
 		} else {
-			// Chuyển hướng đến trang đăng nhập khi chưa đăng nhập
-			window.location.href = "/login";
+		  // Chuyển hướng đến trang đăng nhập khi chưa đăng nhập
+		  window.location.href = "/login";
 		}
-	};
-
+	  };
+	  
+	
 	return (
-		<p className={isLike ? 'text-primary' : ''}>
+		<p className={" " + (isLike ? "text-primary" : " ")}>
 			<i style={{ fontSize: '20px' }} className="fa fa-thumbs-up" onClick={onLikeClick}> {like}</i>
 		</p>
 	);
