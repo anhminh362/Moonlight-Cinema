95511bee58c9e7ce1d9567118574bb7977cd4031
KieuMei2811 <hothikieu28112003@gmail.com>
2023-07-02 22:07:56 +0700

    updated

--- src/Components/ListMovies/Like.js
+++ src/Components/ListMovies/Like.js
@@ -1,17 +1,71 @@
 import React, { useState, useEffect } from 'react';
 
-const Like = () =>{
+const Like = () => {
 	const [like, setLike] = useState(0);
 	const [isLike, setIsLike] = useState(false);
+	const [movies, setMovies] = useState(null);
+	const [users, setUsers] = useState(null);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	const onLikeClick = () =>{
-		setLike(like + (isLike ? -1: 1));
-		setIsLike(!isLike);
-	}
+	useEffect(() => {
+		fetchMovie();
+		fetchUser();
+	}, []);
+
+	const fetchMovie = () => {
+		fetch("http://127.0.0.1:8000/api/movie")
+			.then(response => response.json())
+			.then(movie => {
+				setMovies(movie.id);
+			})
+			.catch(error => console.error('Lỗi khi lấy thông tin phim:', error));
+	};
+
+	const fetchUser = () => {
+		fetch("http://127.0.0.1:8000/api/users")
+			.then(response => response.json())
+			.then(user => {
+				setUsers(user.id);
+				setIsLoggedIn(true); // Đánh dấu là đã đăng nhập thành công
+			})
+			.catch(error => console.error('Lỗi khi lấy thông tin người dùng:', error));
+	};
+
+	const saveLike = () => {
+		if (movies && users) {
+			fetch("http://127.0.0.1:8000/api/like", {
+				method: 'POST',
+				body: JSON.stringify({ 'movie_id': movies, 'user_id': users }),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+				.then(response => response.json())
+				.then(data => {
+					console.log('Lượt thích đã được lưu vào cơ sở dữ liệu:', data);
+				})
+				.catch(error => {
+					console.error('Lỗi khi lưu lượt thích vào cơ sở dữ liệu:', error);
+				});
+		}
+	};
+
+	const onLikeClick = () => {
+		if (isLoggedIn) {
+			setLike(like + (isLike ? -1 : 1));
+			setIsLike(!isLike);
+			saveLike();
+		} else {
+			// Chuyển hướng đến trang đăng nhập khi chưa đăng nhập
+			window.location.href = "/login";
+		}
+	};
+
 	return (
-		<p className={"" + (isLike ? "text-primary": "")}>
-			<i style={{fontSize:"20px"}} className="fa fa-thumbs-up" onClick={onLikeClick}> {like}</i>
+		<p className={isLike ? 'text-primary' : ''}>
+			<i style={{ fontSize: '20px' }} className="fa fa-thumbs-up" onClick={onLikeClick}> {like}</i>
 		</p>
-	)
-}
+	);
+};
+
 export default Like;
