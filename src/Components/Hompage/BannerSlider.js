d0fa14c563505c431724efe86229a8df40eb7b75
thien <thiendam052@gmail.com>
2023-07-08 07:58:22 +0700

    Header responsive

--- src/Components/Hompage/BannerSlider.js
+++ src/Components/Hompage/BannerSlider.js
@@ -11,29 +11,30 @@ const Banner = () => {
   }, []);
 
   return (
-      <div id="slider" className="carousel carousel-light slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          {movies.map((movie, index) => index < 3 && (
-            <div key={movie.id} className={`carousel-item ${index === 0 ? 'active' : ''}`} data-bs-interval="3000">
+    <div id="slider" className="carousel carousel-light slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        {movies.map((movie, index) => index < 3 && (
+          <div key={movie.id} className={`carousel-item ${index === 0 ? 'active' : ''}`} data-bs-interval="3000">
+            <a href={`/Detail/${movie.id}`}>
               <div className="img" style={{ backgroundImage: `url(../picture/${movie.avatar})`, backgroundSize: 'cover' }}></div>
               <div className="carousel-caption d-none d-md-block">
                 <h3 className="text-movie">{movie.name}</h3>
                 <p className="text-movies">{movie.description}<br />
-                  <a href={`/Detail/${movie.id}`}>Movie Detail</a>
                 </p>
               </div>
-            </div>
-          ))}
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#slider" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#slider" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+            </a>
+          </div>
+        ))}
       </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#slider" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#slider" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
   );
 };
 
